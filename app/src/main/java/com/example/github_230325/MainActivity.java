6ccc96a6f4ff0e3320e29da59e7e3f6170a98c72
aqsa1208 <77875831+aqsa1208@users.noreply.github.com>
2023-03-25 15:16:44 +0800

    Text detection

--- app/src/main/java/com/example/github_230325/MainActivity.java
+++ app/src/main/java/com/example/github_230325/MainActivity.java
@@ -3,12 +3,39 @@ package com.example.github_230325;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editText = findViewById(R.id.searchEditText);
+
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                // Do nothing
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                // Update the TextView with the current text in the EditText
+                TextView textView = findViewById(R.id.statusTextView);
+                textView.setText(s.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                // Do nothing
+            }
+        });
     }
 }
